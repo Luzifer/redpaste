8c1629b997d11784385e4822aa48a0ed86804e18
Knut Ahlers <knut@ahlers.me>
2016-09-28 21:27:39 +0200

    Add support for watch / edit

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"time"
 
+	fsnotify "github.com/fsnotify/fsnotify"
 	"gopkg.in/yaml.v2"
 
 	"github.com/Luzifer/rconfig"
@@ -15,11 +18,14 @@ import (
 	"github.com/xuyu/goredis"
 )
 
+const hashSize = 40
+
 var (
 	cfg = struct {
 		RedisConnectString string        `flag:"redis-connect" vardefault:"redis-connect" env:"REDIS_CONNECT" description:"Connection string for the redis server to use"`
 		RedisKey           string        `flag:"redis-key,k" vardefault:"redis-key" description:"Key to write the data to"`
 		TTL                time.Duration `flag:"ttl" vardefault:"ttl" description:"When to expire the key (0=never)"`
+		WatchInterval      time.Duration `flag:"watch-interval" vardefault:"watch-interval" description:"How often to look for changed key"`
 
 		MkConfig bool `flag:"create-config" default:"false" description:"Create a default configuration file"`
 
@@ -27,9 +33,10 @@ var (
 	}{}
 
 	defaultConfig = map[string]string{
-		"redis-connect": "",
-		"redis-key":     "io.luzifer.redpaste",
-		"ttl":           "0",
+		"redis-connect":  "",
+		"redis-key":      "io.luzifer.redpaste",
+		"ttl":            "0",
+		"watch-interval": "2s",
 	}
 
 	version = "dev"
@@ -108,9 +115,46 @@ func initConfig() {
 		log.Fatalf("You need to specify redis connection string")
 	}
 
-	if len(rconfig.Args()) != 2 {
-		fmt.Println("Usage: redpaste <set/get>")
-		os.Exit(1)
+	if len(rconfig.Args()) < 2 {
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Println("Usage: redpaste <set/get>")
+	fmt.Println("       redpaste <watch/edit> <file>")
+	os.Exit(1)
+}
+
+func getAndDecode(rc *goredis.Redis) []byte {
+	hash, err := rc.GetRange(cfg.RedisKey, 0, hashSize-1)
+	if err != nil {
+		log.Fatalf("Unable to read key: %s", err)
+	}
+
+	data, err := rc.GetRange(cfg.RedisKey, hashSize, -1)
+	if err != nil {
+		log.Fatalf("Unable to read key: %s", err)
+	}
+
+	if fmt.Sprintf("%x", sha1.Sum([]byte(data))) != hash {
+		log.Fatalf("Detected a checksum mismatch. Please try again.")
+	}
+
+	rawData, err := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		log.Fatalf("Unable to decode base64 content: %s", err)
+	}
+
+	return rawData
+}
+
+func encodeAndSet(rc *goredis.Redis, data []byte) {
+	value := base64.StdEncoding.EncodeToString(data)
+	hash := fmt.Sprintf("%x", sha1.Sum([]byte(value)))
+
+	if err := rc.Set(cfg.RedisKey, hash+value, int(cfg.TTL.Seconds()), 0, false, false); err != nil {
+		log.Fatalf("Unable to write key: %s", err)
 	}
 }
 
@@ -123,23 +167,103 @@ func main() {
 		log.Fatalf("Unable to connect to redis: %s", err)
 	}
 
-	if action == "get" {
-		data, err := rc.Get(cfg.RedisKey)
-		if err != nil {
-			log.Fatalf("Unable to read key: %s", err)
-		}
-		rawData, err := base64.StdEncoding.DecodeString(string(data))
-		if err != nil {
-			log.Fatalf("Unable to decode base64 content: %s", err)
-		}
-		os.Stdout.Write(rawData)
+	switch action {
+	case "get":
+		os.Stdout.Write(getAndDecode(rc))
 		os.Exit(0)
+
+	case "set":
+		data, err := ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("Unable to read data from stdin: %s", err)
+		}
+
+		encodeAndSet(rc, data)
+
+	case "watch":
+		if len(rconfig.Args()) != 3 {
+			usage()
+		}
+
+		var storedHash string
+
+		for range time.Tick(cfg.WatchInterval) {
+			hash, err := rc.GetRange(cfg.RedisKey, 0, hashSize)
+			if err != nil {
+				log.Fatalf("Unable to read key: %s", err)
+			}
+
+			if hash == storedHash {
+				continue
+			}
+
+			if err := ioutil.WriteFile(rconfig.Args()[2], getAndDecode(rc), 0755); err != nil {
+				log.Fatalf("Unable to write file '%s': %s", rconfig.Args()[2], err)
+			}
+			storedHash = hash
+		}
+
+	case "edit":
+		if len(rconfig.Args()) != 3 {
+			usage()
+		}
+
+		editor := os.Getenv("EDITOR")
+		if editor == "" {
+			log.Fatalf("Evironment variable $EDITOR is unset")
+		}
+
+		watcher, err := fsnotify.NewWatcher()
+		if err != nil {
+			log.Fatalf("Unable to create file system watcher")
+		}
+		defer watcher.Close()
+
+		if _, err := os.Stat(rconfig.Args()[2]); err != nil {
+			if err := ioutil.WriteFile(rconfig.Args()[2], []byte{}, 0644); err != nil {
+				log.Fatalf("Unable to create file '%s': %s", rconfig.Args()[2], err)
+			}
+		}
+
+		doneChan := make(chan error)
+		go func() {
+			cmd := exec.Command(editor, rconfig.Args()[2])
+			cmd.Stdout = os.Stdout
+			cmd.Stdin = os.Stdin
+			cmd.Stderr = os.Stderr
+			cmd.Env = os.Environ()
+			doneChan <- cmd.Run()
+		}()
+
+		if err := watcher.Add(rconfig.Args()[2]); err != nil {
+			log.Fatalf("Unable to watch file: %s", err)
+		}
+
+		for {
+			select {
+			case err := <-doneChan:
+				if err != nil {
+					os.Exit(1)
+				} else {
+					os.Exit(0)
+				}
+
+			case event := <-watcher.Events:
+				switch {
+				case event.Op&fsnotify.Remove == fsnotify.Remove:
+					watcher.Add(event.Name)
+				case event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create:
+					data, err := ioutil.ReadFile(rconfig.Args()[2])
+					if err != nil {
+						log.Fatalf("Unable to read file contents: %s", err)
+					}
+					encodeAndSet(rc, data)
+				}
+			}
+		}
+
+	default:
+		usage()
 	}
 
-	data, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("Unable to read data from stdin: %s", err)
-	}
-
-	rc.Set(cfg.RedisKey, base64.StdEncoding.EncodeToString(data), int(cfg.TTL.Seconds()), 0, false, false)
 }
